a4c1df4ce19000fc5e906dfc3ecca3f4ae93b4de
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-17 14:54:35 -0500

    Make a HalloweenThisYear const and a current year const to show date of HAlloween with dynamic current year. Make test in countdown.test.js file.

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// const calculateDaysUntilHalloween = () => {
-  
-// };
+const DaysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const Countdown = () => {
+  const [currentDay, setCurrentDay] = useState("");
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const dayOfWeek = DaysOfWeek[currentDate.getDay()];
+    const month = Months[currentDate.getMonth()];
+    const day = currentDate.getDate();
+
+    // Calculate Halloween day for the current year.
+
+    const currentYear = currentDate.getFullYear();
+    const halloweenThisYear = new Date(`${currentYear}-10-31`);
+  }, [])
+
   return (
     <p>days until Halloween!</p>
   )
--- src/components/countdown.test.js
+++ src/components/countdown.test.js
@@ -10,8 +10,25 @@ describe("Countdown component", () => {
     // ...no functions
 
     //Assert
-    const DaysElement = screen.getByText("days until Halloween", { exact: false });
+    const DaysElement = screen.getByText("days until Halloween", {
+      exact: false,
+    });
     expect(DaysElement).toBeInTheDocument();
   });
 
+  test("calculates Halloween day for current year", () => {
+    //Arrange
+    render(<Countdown />);
+
+    //Calculate Halloween day for the mock date
+    const mockDate = new Date("2023-08-16T12:00:00Z");
+    global.Date = jest.fn(() => mockDate);
+
+    //Act
+    const currentYear = mockDate.getFullYear();
+    const halloweenThisYear = new Date(`${currentYear}-10-31`);
+
+    // Assert that the calculated Halloween date matches the expected value
+    expect(halloweenThisYear).toEqual(new Date("2023-10-31"));
+  });
 });
