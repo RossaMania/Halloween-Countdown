f8dd9b74d48f3f8e9b1d2dd042762d577f6d5fff
Ross Clettenberg <raclettenberg@gmail.com>
2023-08-18 21:18:44 -0500

    Create halloweenTheme in themes folder. Import into halloweenstatus file. Add fontSize, color, and textAlign key value pairs to halloweenTheme. Import Theme Provider into halloweenstatus.

--- src/components/halloweenstatus.js
+++ src/components/halloweenstatus.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import BoxWrapper from "./box";
+import { ThemeProvider } from "@emotion/react";
+import halloweenTheme from "./../themes/halloweenTheme";
+
 
 const HalloweenStatus = () => {
 
   return (
-    <BoxWrapper>
-      <Typography align="center" variant="h1">
-        It's Halloween!
-      </Typography>
-    </BoxWrapper>
+    <ThemeProvider theme={halloweenTheme}>
+      <BoxWrapper>
+        <Typography>
+          It's Halloween!
+        </Typography>
+      </BoxWrapper>
+    </ThemeProvider>
   );
 
 }
---
+++ src/themes/halloweenTheme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const halloweenTheme = createTheme({
+   typography: {
+    fontSize:"2.5rem",
+    color: "#f57c00",
+    textAlign: "center"
+  }
+});
+
+export default halloweenTheme
